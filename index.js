af15688b724dca0768f8086440bb015396ba0822
Natallia Kostsina <natakostina123@gmail.com>
2022-12-05 11:20:25 +0100

    feat: add greeting

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { parseArgs } from './src/cli/parseArgs.js';
+import { appCli } from './src/cli/cliInterface.js';
 
 const user = parseArgs('username');
+appCli.greet(user)
---
+++ src/cli/cliInterface.js
@@ -0,0 +1,5 @@
+export const appCli = {
+  greet (user) {
+    console.log(`Welcome to the File Manager, ${user}!`);
+  }
+}
